9f0a9c9ce539c24cb7909ffccdc4e624f71cd212
devanshparmar <devanshparmar@EZDI-LAP0094>
2023-05-02 23:37:33 +0530

    main branch

---
+++ jpa/src/main/java/com/example/jpastart/SprintBootH2Application.java
@@ -0,0 +1,19 @@
+package com.example.jpastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@ComponentScan({ "com.example.controller", "com.example.service" })
+@EntityScan("com.example.model")
+@EnableJpaRepositories("com.example.repository")
+@SpringBootApplication
+public class SprintBootH2Application
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SprintBootH2Application.class, args);
+	}
+}
---
+++ jpa/src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,31 @@
+package com.example.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.model.Employee;
+import com.example.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+   @Autowired
+   EmployeeRepository repository;
+
+   public Employee getEmployeeById(int id) {
+      return repository.findById(id).get();
+   }
+   public List<Employee> getAllEmployees(){
+      List<Employee> employees = new ArrayList<>();
+      repository.findAll().forEach(employee -> employees.add(employee));
+      return employees;
+   }
+   public void saveOrUpdate(Employee employee) {
+      repository.save(employee);
+   }
+   public void deleteEmployeeById(int id) {
+      repository.deleteById(id);
+   }
+}
